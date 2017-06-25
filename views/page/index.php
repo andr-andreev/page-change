2f904c491de325f6002936b5c771c22044dbef4e
andr-andreev <andr-andreev@users.noreply.github.com>
2017-06-25 14:42:23 -0500

    Improve pages index view

--- views/page/index.php
+++ views/page/index.php
@@ -1,7 +1,12 @@
 <?php
 
-use yii\helpers\Html;
+use app\models\Category;
+use app\models\Page;
+use app\widgets\ActionColumn;
+use rmrevin\yii\fontawesome\FA;
 use yii\grid\GridView;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\PageSearch */
@@ -21,27 +26,56 @@ $this->params['breadcrumbs'][] = $this->title;
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
             [
+                'attribute' => 'is_active',
+                'format' => 'raw',
+                'filter' => Page::statusList(),
+                'value' => function (Page $data) {
+                    return FA::icon($data->is_active ? FA::_CHECK : FA::_TIMES);
+                },
+                'headerOptions' => [
+                    'width' => 100,
+                    'class' => 'text-center',
+                ],
+                'contentOptions' => ['class' => 'text-center'],
+            ],
+            [
+                'header' => 'Category',
                 'attribute' => 'category_title',
-                'value' => 'category.title'
+                'filter' => ArrayHelper::map(Category::find()->asArray()->all(), 'id', 'title'),
+                'value' => 'category.title',
+                'headerOptions' => [
+                    'width' => 180,
+                ],
             ],
             [
                 'attribute' => 'description',
-                'format' => 'html',
-                'value' => function ($data) {
-                    return Html::a(empty($data->description) ? $data->url : $data->description, $data->url);
+                'format' => 'raw',
+                'value' => function (Page $data) {
+                    $linkName = empty($data->description) ? $data->url : $data->description;
+
+                    return Html::a($linkName, $data->url, ['target' => '_blank']);
                 },
             ],
             [
                 'attribute' => 'filter_from',
                 'label' => 'Filter',
-                'format' => 'html',
-                'value' => function ($data) {
-                    return $data['filter_from'] && $data['filter_from'] ? '<span class="glyphicon glyphicon-ok"></span>' : '';
+                'format' => 'raw',
+                'value' => function (Page $data) {
+                    return FA::icon($data->filter_from && $data->filter_from ? FA::_CHECK : FA::_TIMES);
                 },
+                'headerOptions' => [
+                    'width' => 80,
+                    'class' => 'text-center',
+                ],
+                'contentOptions' => ['class' => 'text-center'],
             ],
             'updated_at:datetime',
-            ['class' => 'yii\grid\ActionColumn'],
+            [
+                'class' => ActionColumn::className(),
+                'template' => '{view} {update} {delete}',
+            ],
         ],
     ]); ?>
 </div>
