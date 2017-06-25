4b3d8bd2b67d10b80c3a7ee421fa4bf409840835
andr-andreev <andr-andreev@users.noreply.github.com>
2017-06-25 14:35:05 -0500

    Add ActionColumn widget

---
+++ widgets/ActionColumn.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace app\widgets;
+
+use rmrevin\yii\fontawesome\FA;
+use Yii;
+use yii\helpers\Html;
+
+class ActionColumn extends \yii\grid\ActionColumn
+{
+    public $header = 'Actions';
+    public $buttonOptions = ['class' => 'btn btn-info btn-rounded btn-condensed btn-sm'];
+    public $headerOptions = ['width' => 240, 'class' => 'text-center'];
+    public $contentOptions = ['class' => 'text-center'];
+
+    protected function initDefaultButtons()
+    {
+        if (!isset($this->buttons['view'])) {
+            $this->buttons['view'] = function ($url, $model, $key) {
+                $title = Yii::t('yii', 'View');
+                $options = [
+                    'class' => 'btn btn-info btn-rounded btn-condensed btn-sm',
+                    'title' => $title,
+                    'aria-label' => $title,
+                    'data-pjax' => '0',
+                ];
+                return Html::a(FA::icon(FA::_EYE) . ' ' . $title, $url, $options);
+            };
+        }
+
+        if (!isset($this->buttons['update'])) {
+            $this->buttons['update'] = function ($url, $model, $key) {
+                $title = Yii::t('yii', 'Update');
+                $options = [
+                    'class' => 'btn btn-success btn-rounded btn-condensed btn-sm',
+                    'title' => $title,
+                    'aria-label' => $title,
+                    'data-pjax' => '0',
+                ];
+                return Html::a(FA::icon(FA::_PENCIL) . ' ' . $title, $url, $options);
+            };
+        }
+
+        if (!isset($this->buttons['delete'])) {
+            $this->buttons['delete'] = function ($url, $model, $key) {
+                $title = Yii::t('yii', 'Delete');
+                $options = [
+                    'class' => 'btn btn-danger btn-rounded btn-condensed btn-sm',
+                    'title' => $title,
+                    'aria-label' => $title,
+                    'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                    'data-method' => 'post',
+                    'data-pjax' => '0',
+                ];
+                return Html::a(FA::icon(FA::_TIMES) . ' ' . $title, $url, $options);
+            };
+        }
+
+        parent::initDefaultButtons();
+    }
+}
