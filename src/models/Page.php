ae199cb10f6a661893fb0262a76fb61481e8065b
Andrey Andreev <andr-andreev@users.noreply.github.com>
2018-08-11 12:03:08 -0500

    Add const visibility

--- src/models/Page.php
+++ src/models/Page.php
@@ -22,8 +22,8 @@ use yii\behaviors\TimestampBehavior;
  */
 class Page extends \yii\db\ActiveRecord
 {
-    const STATUS_INACTIVE = 0;
-    const STATUS_ACTIVE = 1;
+    public const STATUS_INACTIVE = 0;
+    public const STATUS_ACTIVE = 1;
 
     /**
      * @inheritdoc
