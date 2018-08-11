353f710e250f7d3ba894f5812d17ecd8ce2774cc
Andrey Andreev <andr-andreev@users.noreply.github.com>
2018-08-11 12:13:51 -0500

    Remove property with the same default value

--- src/assets/AppAsset.php
+++ src/assets/AppAsset.php
@@ -19,11 +19,11 @@ class AppAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
+
     public $css = [
         'css/site.css',
     ];
-    public $js = [
-    ];
+
     public $depends = [
         YiiAsset::class,
         BootstrapAsset::class,
